82e30008c2284de8942f32b5749e7808d4812c69
NemanjaKasanov <NemanjaKasanov@users.noreply.github.com>
2020-04-08 20:00:35 +0200

    change1

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
     $.ajax({
         url: "elements.json",
-        success: function(data){
+        success: function(data) {
             localStorage.setItem("left", JSON.stringify(data));
         },
-        error: function(err){
+        error: function(err) {
             console.log(err);
         }
     });
@@ -16,55 +16,54 @@ $(document).ready(function(){
     displayElements(arrayLeft, "left");
 
 
-    $("#add").click(function(){
+    $("#add").click(function() {
         let checked = [];
         let unchecked = [];
         let elementsArray = JSON.parse(localStorage.getItem("left"));
 
-        $(".cbx:checked").each(function(){
+        $(".cbx:checked").each(function() {
             checked.push($(this).attr("data"));
         });
 
         let allIds = [];
-        elementsArray.forEach(function(el){
+        elementsArray.forEach(function(el) {
             allIds.push(el.id);
         });
 
-        unchecked = allIds.filter(function(id){
-            if(!checked.includes(id)){
+        unchecked = allIds.filter(function(id) {
+            if (!checked.includes(id)) {
                 return true;
             }
         });
 
         let rightArray = [];
-        if(localStorage.getItem("right")){
+        if (localStorage.getItem("right")) {
             let elementsAlreadyin = JSON.parse(localStorage.getItem("right"));
 
-            rightArray = elementsArray.filter(function(el){
-                for(let num of checked){
-                    if(el.id == num){
+            rightArray = elementsArray.filter(function(el) {
+                for (let num of checked) {
+                    if (el.id == num) {
                         return true;
                     }
                 }
             });
 
-            elementsAlreadyin.forEach(function(el){
+            elementsAlreadyin.forEach(function(el) {
                 rightArray.push(el);
             });
-        }
-        else{
-            rightArray = elementsArray.filter(function(el){
-                for(let num of checked){
-                    if(el.id == num){
+        } else {
+            rightArray = elementsArray.filter(function(el) {
+                for (let num of checked) {
+                    if (el.id == num) {
                         return true;
                     }
                 }
             });
         }
 
-        let leftArray = elementsArray.filter(function(el){
-            for(let num of unchecked){
-                if(el.id == num){
+        let leftArray = elementsArray.filter(function(el) {
+            for (let num of unchecked) {
+                if (el.id == num) {
                     return true;
                 }
             }
@@ -77,32 +76,114 @@ $(document).ready(function(){
 
         localStorage.setItem("left", JSON.stringify(leftArray));
         localStorage.setItem("right", JSON.stringify(rightArray));
-    }); 
-
-    $("#remove").click(function(){
-        
     });
+
+    $("#remove").click(function() {
+
+        let checked = [];
+        let unchecked = [];
+        let elementsArray = JSON.parse(localStorage.getItem("right"));
+
+        $(".cbx:checked").each(function() {
+            checked.push($(this).attr("data"));
+        });
+
+        let allIds = [];
+        elementsArray.forEach(function(el) {
+            allIds.push(el.id);
+        });
+
+        unchecked = allIds.filter(function(id) {
+            if (!checked.includes(id)) {
+                return true;
+            }
+        });
+
+        let leftArray = [];
+        if (localStorage.getItem("left")) {
+            let elementsAlreadyin = JSON.parse(localStorage.getItem("left"));
+
+            leftArray = elementsArray.filter(function(el) {
+                for (let num of checked) {
+                    if (el.id == num) {
+                        return true;
+                    }
+                }
+            });
+
+            elementsAlreadyin.forEach(function(el) {
+                leftArray.push(el);
+            });
+        } else {
+            leftArray = elementsArray.filter(function(el) {
+                for (let num of checked) {
+                    if (el.id == num) {
+                        return true;
+                    }
+                }
+            });
+        }
+
+        let rightArray = elementsArray.filter(function(el) {
+            for (let num of unchecked) {
+                if (el.id == num) {
+                    return true;
+                }
+            }
+        });
+
+        sortElements(leftArray);
+
+        displayElements(leftArray, "left");
+        displayElements(rightArray, "right");
+
+        localStorage.setItem("left", JSON.stringify(leftArray));
+        localStorage.setItem("right", JSON.stringify(rightArray));
+    });
+
+    $("#submit").click(function() {
+        let arrayToSend = JSON.parse(localStorage.getItem("right"));
+
+        if (arrayToSend) {
+            $.ajax({
+                url: "obrada.php",
+                method: "GET",
+                type: "json",
+                data: {
+                    products: arrayToSend
+                },
+                success: function(data) {
+                    alert("Uspesan unos!");
+                },
+                error: function(err) {
+                    console.error(err);
+                }
+            });
+        } else {
+            alert("There must be elements in the cart to proceed.");
+        }
+    });
+
+
 
 });
 
-function sortElements(array){
-    array.sort(function(a, b){
-        if(a.id < b.id){
+function sortElements(array) {
+    array.sort(function(a, b) {
+        if (a.id < b.id) {
             return -1;
-        }
-        else if(a.id > b.id){
+        } else if (a.id > b.id) {
             return 1;
-        }
-        else{
+        } else {
             return 0;
         }
     });
 }
 
-function displayElements(data, location){
+function displayElements(data, location) {
     let ispis = "";
 
-    data.forEach(function(el){
+    data.forEach(function(el) {
         ispis += `
             <div class="custom-control custom-checkbox mr-sm-2">
                 <input type="checkbox" class="custom-control-input cbx" data="${el.id}" id="${el.id}">
@@ -111,10 +192,9 @@ function displayElements(data, location){
         `;
     });
 
-    if(location == "left"){
+    if (location == "left") {
         $("#listLeft").html(ispis);
-    }
-    else if(location == "right"){
+    } else if (location == "right") {
         $("#listRight").html(ispis);
     }
 }
