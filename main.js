d75e250ed0126823b8ad52bcb748f057b4feb987
NemanjaKasanov <49758709+NemanjaKasanov@users.noreply.github.com>
2020-04-08 19:20:43 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,120 @@
+$(document).ready(function(){
+
+    $.ajax({
+        url: "elements.json",
+        success: function(data){
+            localStorage.setItem("left", JSON.stringify(data));
+        },
+        error: function(err){
+            console.log(err);
+        }
+    });
+
+    localStorage.removeItem("right");
+
+    let arrayLeft = JSON.parse(localStorage.getItem("left"));
+    displayElements(arrayLeft, "left");
+
+
+    $("#add").click(function(){
+        let checked = [];
+        let unchecked = [];
+        let elementsArray = JSON.parse(localStorage.getItem("left"));
+
+        $(".cbx:checked").each(function(){
+            checked.push($(this).attr("data"));
+        });
+
+        let allIds = [];
+        elementsArray.forEach(function(el){
+            allIds.push(el.id);
+        });
+
+        unchecked = allIds.filter(function(id){
+            if(!checked.includes(id)){
+                return true;
+            }
+        });
+
+        let rightArray = [];
+        if(localStorage.getItem("right")){
+            let elementsAlreadyin = JSON.parse(localStorage.getItem("right"));
+
+            rightArray = elementsArray.filter(function(el){
+                for(let num of checked){
+                    if(el.id == num){
+                        return true;
+                    }
+                }
+            });
+
+            elementsAlreadyin.forEach(function(el){
+                rightArray.push(el);
+            });
+        }
+        else{
+            rightArray = elementsArray.filter(function(el){
+                for(let num of checked){
+                    if(el.id == num){
+                        return true;
+                    }
+                }
+            });
+        }
+
+        let leftArray = elementsArray.filter(function(el){
+            for(let num of unchecked){
+                if(el.id == num){
+                    return true;
+                }
+            }
+        });
+
+        sortElements(rightArray);
+
+        displayElements(leftArray, "left");
+        displayElements(rightArray, "right");
+
+        localStorage.setItem("left", JSON.stringify(leftArray));
+        localStorage.setItem("right", JSON.stringify(rightArray));
+    }); 
+
+    $("#remove").click(function(){
+        
+    });
+
+});
+
+function sortElements(array){
+    array.sort(function(a, b){
+        if(a.id < b.id){
+            return -1;
+        }
+        else if(a.id > b.id){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    });
+}
+
+function displayElements(data, location){
+    let ispis = "";
+
+    data.forEach(function(el){
+        ispis += `
+            <div class="custom-control custom-checkbox mr-sm-2">
+                <input type="checkbox" class="custom-control-input cbx" data="${el.id}" id="${el.id}">
+                <label class="custom-control-label"  for="${el.id}">${el.name}</label>
+            </div>
+        `;
+    });
+
+    if(location == "left"){
+        $("#listLeft").html(ispis);
+    }
+    else if(location == "right"){
+        $("#listRight").html(ispis);
+    }
+}
